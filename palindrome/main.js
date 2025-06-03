0f90227492cd8a20c41e33c7c613fdfe894243d7
pasqualedimaro <pakydm15@gmail.com>
2025-06-03 16:25:23 +0200

    open for cicle

--- palindrome/main.js
+++ palindrome/main.js
@@ -6,7 +6,11 @@ Creare una funzione per capire se la parola inserita è palindroma*/
 //funzione per verificare se una parola è palindroma
 // una parola è palindroma se si legge uguale da sx a dx e viceversa
 
-function isPalindrome(world){
+function isPalindrome(word){
     //converto la parola in minuscolo perchè cosi ignoro le differenze maiuscole/minuscole
-    world = world.toLowerCase();
+    word = word.toLowerCase();
+    // apro un ciclo for che confronta i caratteri, primo/ultimo secondo/penultimo
+     for (let i = 0; i < word.length / 2; i++) {
+
+     }
 }

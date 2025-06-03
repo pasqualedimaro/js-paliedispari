acde6569ed37e56f1b481f19d18fd35941b8e76f
pasqualedimaro <pakydm15@gmail.com>
2025-06-03 16:21:49 +0200

    scvaffolding, open a fuinction and add lowercase

---
+++ palindrome/main.js
@@ -0,0 +1,12 @@
+
+    /*Palidroma
+Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita è palindroma*/
+
+//funzione per verificare se una parola è palindroma
+// una parola è palindroma se si legge uguale da sx a dx e viceversa
+
+function isPalindrome(world){
+    //converto la parola in minuscolo perchè cosi ignoro le differenze maiuscole/minuscole
+    world = world.toLowerCase();
+}
